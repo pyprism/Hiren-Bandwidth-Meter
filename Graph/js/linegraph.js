24c4ede52214607f644890d0a95ec1a3b74b0124
Wasi Mohammed Abdullah <wasi0013@gmail.com>
2014-06-05 13:04:35 +0600

    Added more comments

--- Graph/js/linegraph.js
+++ Graph/js/linegraph.js
@@ -1,9 +1,10 @@
-
+        //test data it will be passed to the function
 		var lineChartData = {
-			//vua data :P
+		    //time labels
 			labels : ["1s","2s","3s","4s","5s","6s","7s"],
 			datasets : [
 				{
+					//for download
 					fillColor : "rgba(220,220,220,0.5)",
 					strokeColor : "rgba(220,220,220,1)",
 					pointColor : "rgba(220,220,220,1)",
@@ -11,6 +12,7 @@
 					data : [65,59,90,81,56,55,40]
 				},
 				{
+					// for upload
 					fillColor : "rgba(151,187,205,0.5)",
 					strokeColor : "rgba(151,187,205,1)",
 					pointColor : "rgba(151,187,205,1)",
@@ -20,6 +22,6 @@
 			]
 			
 		}
-
+    //function calls :P
 	var myLine = new Chart(document.getElementById("canvas").getContext("2d")).Line(lineChartData);
 	
